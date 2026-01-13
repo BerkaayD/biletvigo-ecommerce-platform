e16ff2b4db94a37e34b95223cbb91349fcae303c
Berkay Demirci <143441795+BerkaayD@users.noreply.github.com>
2026-01-13 16:43:20 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // 'sans' ailesini güncelleyerek Segoe UI'ı başa ekliyoruz
+   
         sans: ['"Segoe UI"', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
       },
     },
